43f2418d0bb4c86ae533a0c7ae20107151b0a7f7
kapc0003 <kapc0003@algonquinlive.com>
2017-04-12 13:36:22 -0400

    Upload Files

---
+++ platforms/ios/reviewr.xcarchive/Products/Applications/reviewr.app/www/js/index.js
@@ -0,0 +1,244 @@
+/*****************************************************************
+File: index.js
+Author: Alison Kapcala
+Description: ReviewR Assignment - iOS Cordova App
+
+Version: 0.0.1
+Updated: April 12, 2017
+
+*****************************************************************/
+//global declarations
+var rating = 0;
+var stars = null;
+let imageFile;
+let reviewList = new Array();
+let currentReview = 0;
+//localStorage.removeItem("reviewr-kapc0003");
+document.addEventListener('deviceready', onDeviceReady);
+
+function onDeviceReady(){
+    console.log("Ready");
+    
+    let saveButton = document.getElementById("saveBtn");
+    saveButton.addEventListener("click", saveReview);
+    
+    let cancelButton = document.getElementById("cancelBtn");
+    cancelButton.addEventListener("click", cancelModal);
+    
+    let pictureButton = document.getElementById("pictureBtn");
+    pictureButton.addEventListener("click", takePicture);
+    
+    let deleteButton = document.getElementById("deleteBtn");
+    deleteButton.addEventListener("click", deleteReview);
+    
+    setStars();
+    displayReviews();
+    
+}
+
+function cancelModal(){
+    document.getElementById("itemName").value = "";
+    value = 0;
+    document.getElementById("myImage").src = "";
+    setStars();
+    
+    var endEvent = new CustomEvent('touchend', {bubbles:true, cancelable:true});
+    var a = document.querySelector("a#xButton");
+    a.dispatchEvent(endEvent);
+}
+
+function saveReview(){
+    let itemNameToBeSaved = document.getElementById("itemName").value;
+    let ratingToBeSaved = rating;
+    imageFile = document.getElementById("myImage").src;
+    let timeStamp = new Date().getTime() / 1000;
+    
+    let review = {id: timeStamp,
+                  name:itemNameToBeSaved,
+                  rating:ratingToBeSaved,
+                  img:imageFile,
+                 };
+    
+    reviewList.push(review);
+    SaveToLS();
+    cancelModal();
+    displayReviews();
+}
+
+function takePicture(){
+    var options = {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        encodingType: Camera.EncodingType.PNG,
+        mediaType: Camera.MediaType.PICTURE,
+        pictureSourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        targetWidth: 300,
+        targetHeight: 300
+    }
+    navigator.camera.getPicture( onSuccess, onFail, options ); 
+}
+
+function displayReviews(){
+    RetrieveFromLS();
+
+    let list = document.getElementById("review-list");
+    list.innerHTML = "";
+    
+    let length = reviewList.length;
+    
+    for(let i = 0; i < length; i++){
+        
+        let li = document.createElement("li");
+        li.className = "table-view-cell media";
+        li.setAttribute("dataId", reviewList[i].id);
+        
+        let a = document.createElement("a");
+        a.href = "#deleteReview"
+        a.classList = "navigate-right";
+        
+        let div = document.createElement("div");
+        div.classList = "media-body";
+        
+        let pName = document.createElement("p");
+        pName.className = "name";
+        pName.textContent = reviewList[i].name;
+        
+        let img = document.createElement("img");
+        img.classList = "media-object pull-left";
+        img.src = reviewList[i].img;
+        img.id = "displayedImage";
+        
+        div.appendChild(pName);
+        
+        console.log("review list rating");
+        console.log(reviewList[i].rating);
+        
+        let starLength = reviewList[i].rating;
+        
+        for(let n = 0; n < starLength; n++){
+            let spanRating = document.createElement("span");
+            spanRating.classList = "star";
+            //spanRating.textContent = "&nbsp;";
+            div.appendChild(spanRating);
+        }
+        
+        a.addEventListener("touchstart", openReview);
+        
+        a.appendChild(img);
+        a.appendChild(div);
+        li.appendChild(a);
+        list.appendChild(li);
+    }
+}
+
+function openReview(ev){
+    
+    console.log(ev);
+    console.log(ev.target.parentElement.attributes.dataId.nodeValue);
+    currentReview = ev.target.parentElement.attributes.dataId.nodeValue;
+    console.log(currentReview);
+    
+    document.getElementById("closeDeleteMenu").addEventListener("touchstart", function(){
+        document.getElementById("itemNameDisplay").textContent = "";
+        document.getElementById("reviewImageDisplay").src = "";
+        value = 0;
+    })
+    
+    
+    let length = reviewList.length;
+    for(let i = 0; i < length; i++){
+        if(currentReview == reviewList[i].id){
+            document.getElementById("itemNameDisplay").textContent = "Item: " + reviewList[i].name;
+            document.getElementById("reviewImageDisplay").src = reviewList[i].img;
+            document.getElementById("starsCurrent").innerHTML="";
+            let lengthStars = reviewList[i].rating;
+            for(let n = 0; n < lengthStars; n++){
+                let spanRating = document.createElement("span");
+                spanRating.classList = "star";
+                document.getElementById("starsCurrent").appendChild(spanRating);
+            }
+            break;
+        }
+    }
+}
+
+function deleteReview(){
+    let length = reviewList.length;
+    for(let i = 0; i < length; i++){
+        console.log(currentReview);
+        console.log(reviewList[i].id);
+        if(currentReview == reviewList[i].id){
+            reviewList.splice(i,1);
+            console.log(reviewList);
+            break;
+        }
+    }
+    
+    document.getElementById("itemNameDisplay").textContent = "";
+    document.getElementById("reviewImageDisplay").src = "";
+    document.getElementById("starsCurrent").innerHTML = "";
+    value = 0;
+    
+    SaveToLS();
+    displayReviews();
+    
+    var endEvent = new CustomEvent('touchend', {bubbles:true, cancelable:true});
+    var a = document.querySelector("#closeDeleteMenu");
+    a.dispatchEvent(endEvent);
+}
+
+function setStars(){
+    stars = document.querySelectorAll('.star');
+    addListeners();
+    setRating(); 
+}
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+function addListeners(){
+  [].forEach.call(stars, function(star, index){
+    star.addEventListener('click', (function(idx){
+      console.log('adding listener', index);
+      return function(){
+        rating = idx + 1;  
+        console.log('Rating is now', rating)
+        setRating();
+      }
+    })(index));
+  });
+  
+}
+
+function setRating(){
+  [].forEach.call(stars, function(star, index){
+    if(rating > index){
+      star.classList.add('rated');
+      console.log('added rated on', index );
+    }else{
+      star.classList.remove('rated');
+      console.log('removed rated on', index );
+    }
+  });
+}
+
+function SaveToLS(){
+    localStorage.setItem("reviewr-kapc0003", JSON.stringify(reviewList));
+}
+
+function RetrieveFromLS(){
+    if(!localStorage.getItem("reviewr-kapc0003")){
+        console.log("No data found");
+    }
+    else{
+        reviewList = JSON.parse(localStorage.getItem("reviewr-kapc0003"));
+        console.log("Data retrived from LS");
+    }
+}
